154dc2357114d9534e809f33019d26afa92f9917
seulda <kim02977@naver.com>
2021-09-13 18:18:32 +0900

    07-2) css styling

--- src/Movie.js
+++ src/Movie.js
@@ -10,12 +10,13 @@ function Movie({ title, year, summary, poster, genres }) {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul classname="novie__genres">
+                <ul className="movie__genres">
                     {genres.map((genre, index) => {
                         return <li key={index} className="movie__genre">{genre}</li>
                     })}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                {/* [문자열].slice(시작, 끝) : 문자열 글자수 제한 */}
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
